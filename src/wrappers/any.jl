5ca51057284dd8072d7db565bd70abda8a1fbf12
jlbuild <juliabuildbot@gmail.com>
2020-10-17 01:49:32 +0000

    adwaita_icon_theme_jll build 3.33.92+5

--- src/wrappers/any.jl
+++ src/wrappers/any.jl
@@ -2,45 +2,14 @@
 export adwaita_icons_dir
 
 using hicolor_icon_theme_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `adwaita_icons_dir`
-const adwaita_icons_dir_splitpath = ["share", "icons"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-adwaita_icons_dir_path = ""
-
-# adwaita_icons_dir-specific global declaration
-# This will be filled out by __init__()
-adwaita_icons_dir = ""
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("adwaita_icon_theme")
+JLLWrappers.@declare_file_product(adwaita_icons_dir)
 function __init__()
-    global artifact_dir = abspath(artifact"adwaita_icon_theme")
+    JLLWrappers.@generate_init_header(hicolor_icon_theme_jll)
+    JLLWrappers.@init_file_product(
+        adwaita_icons_dir,
+        "share/icons",
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (hicolor_icon_theme_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (hicolor_icon_theme_jll.LIBPATH_list,))
-
-    global adwaita_icons_dir_path = normpath(joinpath(artifact_dir, adwaita_icons_dir_splitpath...))
-
-    global adwaita_icons_dir = adwaita_icons_dir_path
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
