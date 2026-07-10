4f2ae18f58dab7c308740853469834488fe49393
jlbuild <juliabuildbot@gmail.com>
2026-07-10 06:13:34 +0000

    adwaita_icon_theme_jll build 43.0.1+1

--- src/adwaita_icon_theme_jll.jl
+++ src/adwaita_icon_theme_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("adwaita_icon_theme")
-JLLWrappers.@generate_main_file("adwaita_icon_theme", UUID("b437f822-2cd6-5e08-a15c-8bac984d38ee"))
+JLLWrappers.@generate_main_file("adwaita_icon_theme", Base.UUID("b437f822-2cd6-5e08-a15c-8bac984d38ee"))
 end  # module adwaita_icon_theme_jll
